128ef8e88eafa6772c678af6e20978477cd27d77
prabhash4772@gmail.com <prabhash4772@gmail.com>
2024-05-06 13:18:27 +0530

    Extends code

--- Class.java
+++ Class.java
@@ -1,5 +1,13 @@
 public class Class {
-    public static void main(String[] args) {
+    String Name;
+    int roll;
 
+    public Class(String Name, int roll) {
+        this.Name = Name;
+        this.roll = roll;
+    }
+    
+    public static void main(String[] args) {
+      
     }
 }
